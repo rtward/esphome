508d3fd43287da1a239a9e78254e95f70fb7eaef
Robert Ward <robert@rtward.com>
2021-06-22 21:07:37 -0500

    change library

--- esphome/components/wiegand_reader/wiegand_reader.cpp
+++ esphome/components/wiegand_reader/wiegand_reader.cpp
@@ -1,61 +1,202 @@
 #include "wiegand_reader.h"
 #include "esphome/core/log.h"
+#include "esphome/core/helpers.h"
+
+/*
+ * Most of the code in this library has been adapted from
+ *
+ * https://github.com/monkeyboard/Wiegand-Protocol-Library-for-Arduino
+ *
+ * And was copied out on 2021-06-22
+ *
+ * If bugs are found in this implementation, they should not be reported to the library listed, but it may be worth
+ * reviewing changes to the library after that point and attempting to port them into here.
+ */
 
 namespace esphome {
 namespace wiegand_reader {
 
 static const char *TAG = "wiegand_reader";
 
-void ICACHE_RAM_ATTR WiegandReader::pin_state_changed_(WiegandReader *reader) {
-  reader->wiegand_.setPin0State(reader->pin_d0_->digital_read());
-  reader->wiegand_.setPin1State(reader->pin_d1_->digital_read());
+void WiegandReader::read_d0_(WiegandReader *reader) {
+  reader->_bitCount++;				// Increament bit count for Interrupt connected to D0
+  if (reader->_bitCount>31)			// If bit count more than 31, process high bits
+  {
+    reader->_cardTempHigh |= ((0x80000000 & reader->_cardTemp)>>31);	//	shift value to high bits
+    reader->_cardTempHigh <<= 1;
+    reader->_cardTemp <<=1;
+  }
+  else
+  {
+    reader->_cardTemp <<= 1;		// D0 represent binary 0, so just left shift card data
+  }
+  reader->_lastWiegand = millis();	// Keep track of last wiegand bit received
 }
 
-void WiegandReader::received_data_(uint8_t* data, uint8_t bits, WiegandReader *reader) {
-  uint8_t byte_count = (bits + 7) / 8;
-
-  String code = "";
-  for (int i=0; i<byte_count; i++) {
-    code = code + String(data[i], 16);
+void WiegandReader::read_d1_(WiegandReader *reader) {
+  reader->_bitCount ++;				// Increment bit count for Interrupt connected to D1
+  if (reader->_bitCount>31)			// If bit count more than 31, process high bits
+  {
+    reader->_cardTempHigh |= ((0x80000000 & reader->_cardTemp)>>31);	// shift value to high bits
+    reader->_cardTempHigh <<= 1;
+    reader->_cardTemp |= 1;
+    reader->_cardTemp <<=1;
   }
-  ESP_LOGD(TAG, "Data received : %s", code.c_str());
-
-  for (auto *trigger : reader->triggers_)
-    trigger->process(code);
+  else
+  {
+    reader->_cardTemp |= 1;			// D1 represent binary 1, so OR card data with 1 then
+    reader->_cardTemp <<= 1;		// left shift card data
+  }
+  reader->_lastWiegand = millis();	// Keep track of last wiegand bit received
 }
 
-void WiegandReader::received_data_error_(Wiegand::DataError error, uint8_t* raw_data, uint8_t raw_bits, const char* message) {
-  ESP_LOGE(TAG, "FAILED : %s", message);
-  ESP_LOGE(TAG, "   ERROR : %s", Wiegand::DataErrorStr(error));
+char WiegandReader::translate_enter_escape_keypress_(char originalKeyPress) {
+  switch(originalKeyPress) {
+    case 0x0b:        // 11 or * key
+      return 0x0d;  // 13 or ASCII ENTER
 
-  uint8_t byte_count = (raw_bits + 7) / 8;
-  ESP_LOGE(TAG, "   BYTE COUNT : %i", byte_count);
+    case 0x0a:        // 10 or # key
+      return 0x1b;  // 27 or ASCII ESCAPE
 
-  String code = "";
-  for (int i=0; i<byte_count; i++) {
-    code = code + String(raw_data[i], 16);
+    default:
+      return originalKeyPress;
   }
-  ESP_LOGE(TAG, "   DECODED : %s", code.c_str());
+}
+
+unsigned long WiegandReader::get_card_id_(volatile unsigned long *codehigh, volatile unsigned long *codelow, char bitlength) {
+  if (bitlength==26)								// EM tag
+    return (*codelow & 0x1FFFFFE) >>1;
+
+  if (bitlength==24)
+    return (*codelow & 0x7FFFFE) >>1;
+
+  if (bitlength==34)								// Mifare
+  {
+    *codehigh = *codehigh & 0x03;				// only need the 2 LSB of the codehigh
+    *codehigh <<= 30;							// shift 2 LSB to MSB
+    *codelow >>=1;
+    return *codehigh | *codelow;
+  }
+
+  if (bitlength==32) {
+    return (*codelow & 0x7FFFFFFE ) >>1;
+  }
+
+  return *codelow;								// EM tag or Mifare without parity bits
+}
+
+bool WiegandReader::do_wiegand_conversion_() {
+  unsigned long cardID;
+  unsigned long sysTick = millis();
+
+  if ((sysTick - this->_lastWiegand) > 25)								// if no more signal coming through after 25ms
+  {
+    if ((this->_bitCount==24) || (this->_bitCount==26) || (this->_bitCount==32) || (this->_bitCount==34) || (this->_bitCount==8) || (this->_bitCount==4)) 	// bitCount for keypress=4 or 8, Wiegand 26=24 or 26, Wiegand 34=32 or 34
+    {
+      this->_cardTemp >>= 1;			// shift right 1 bit to get back the real value - interrupt done 1 left shift in advance
+      if (this->_bitCount>32)			// bit count more than 32 bits, shift high bits right to make adjustment
+        this->_cardTempHigh >>= 1;
+
+      if (this->_bitCount==8)		// keypress wiegand with integrity
+      {
+        // 8-bit Wiegand keyboard data, high nibble is the "NOT" of low nibble
+        // eg if key 1 pressed, data=E1 in binary 11100001 , high nibble=1110 , low nibble = 0001
+        char highNibble = (this->_cardTemp & 0xf0) >>4;
+        char lowNibble = (this->_cardTemp & 0x0f);
+        this->_wiegandType=_bitCount;
+        this->_bitCount=0;
+        this->_cardTemp=0;
+        this->_cardTempHigh=0;
+
+        if (lowNibble == (~highNibble & 0x0f))		// check if low nibble matches the "NOT" of high nibble.
+        {
+          this->_code = (int)translate_enter_escape_keypress_(lowNibble);
+          return true;
+        }
+        else {
+          this->_lastWiegand=sysTick;
+          this->_bitCount=0;
+          this->_cardTemp=0;
+          this->_cardTempHigh=0;
+          return false;
+        }
+
+        // TODO: Handle validation failure case!
+      }
+      else if (4 == this->_bitCount) {
+        // 4-bit Wiegand codes have no data integrity check so we just
+        // read the LOW nibble.
+        this->_code = (int)translate_enter_escape_keypress_(_cardTemp & 0x0000000F);
+
+        this->_wiegandType = _bitCount;
+        this->_bitCount = 0;
+        this->_cardTemp = 0;
+        this->_cardTempHigh = 0;
+
+        return true;
+      }
+      else		// wiegand 26 or wiegand 34
+      {
+        cardID = get_card_id_(&this->_cardTempHigh, &this->_cardTemp, this->_bitCount);
+        this->_wiegandType=_bitCount;
+        this->_bitCount=0;
+        this->_cardTemp=0;
+        this->_cardTempHigh=0;
+        this->_code=cardID;
+        return true;
+      }
+    }
+    else
+    {
+      // well time over 25 ms and bitCount !=8 , !=26, !=34 , must be noise or nothing then.
+      this->_lastWiegand=sysTick;
+      this->_bitCount=0;
+      this->_cardTemp=0;
+      this->_cardTempHigh=0;
+      return false;
+    }
+  }
+  else
+    return false;
 }
 
 void WiegandReader::setup() {
   this->pin_d0_->pin_mode(INPUT);
   this->pin_d1_->pin_mode(INPUT);
-  this->wiegand_.onReceive(WiegandReader::received_data_, this);
-  this->wiegand_.onReceiveError(WiegandReader::received_data_error_, "Card read error: ");
+
+  _lastWiegand = 0;
+  _cardTempHigh = 0;
+  _cardTemp = 0;
+  _code = 0;
+  _wiegandType = 0;
+  _bitCount = 0;
 
   this->dump_config();
 
-  this->wiegand_.begin(0xFF, true);
+  this->pin_d0_->attach_interrupt(WiegandReader::read_d0_, this, FALLING);
+  this->pin_d1_->attach_interrupt(WiegandReader::read_d1_, this, FALLING);
+}
 
-  this->pin_d0_->attach_interrupt(WiegandReader::pin_state_changed_, this, CHANGE);
-  this->pin_d1_->attach_interrupt(WiegandReader::pin_state_changed_, this, CHANGE);
-  this->pin_state_changed_(this);
+std::string WiegandReader::get_code_hex_(std::string rawCode) {
+  std::size_t dataStart = rawCode.find_first_not_of("0");
+  if (dataStart != std::string::npos) {
+    return to_lowercase_underscore(rawCode.substr(dataStart));
+  } else {
+    return to_lowercase_underscore(rawCode);
+  }
 }
 
 void WiegandReader::update() {
   InterruptLock lock;
-  this->wiegand_.flush();
+  if (this->do_wiegand_conversion_()) {
+    std::string code = get_code_hex_(uint64_to_string(this->_code));
+
+    ESP_LOGD(TAG, "Data received : %s", code.c_str());
+
+    for (auto *trigger : this->triggers_) {
+      trigger->process(code);
+    }
+  }
 }
 
 void WiegandReader::set_data_pins(GPIOPin *pin_d0, GPIOPin *pin_d1){
@@ -72,7 +213,7 @@ void WiegandReader::dump_config() {
   LOG_UPDATE_INTERVAL(this);
 }
 
-void WiegandReaderTrigger::process(String tag) {
+void WiegandReaderTrigger::process(std::string tag) {
   this->trigger(tag.c_str());
 }
 
--- esphome/components/wiegand_reader/wiegand_reader.h
+++ esphome/components/wiegand_reader/wiegand_reader.h
@@ -2,14 +2,13 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
-#include "Wiegand.h"
 
 namespace esphome {
 namespace wiegand_reader {
 
 class WiegandReaderTrigger : public Trigger<std::string> {
  public:
-  void process(String tag);
+  void process(std::string tag);
 };
 
 class WiegandReader : public PollingComponent {
@@ -22,13 +21,25 @@ class WiegandReader : public PollingComponent {
   void register_trigger(WiegandReaderTrigger *trig) { this->triggers_.push_back(trig); }
 
  protected:
-  static void received_data_(uint8_t* data, uint8_t bits, WiegandReader *reader);
-  static void pin_state_changed_(WiegandReader *reader);
-  static void received_data_error_(Wiegand::DataError error, uint8_t* raw_data, uint8_t raw_bits, const char* message);
-  Wiegand wiegand_;
+  unsigned long get_code_();
+  bool do_wiegand_conversion_();
+
   GPIOPin *pin_d0_;
   GPIOPin *pin_d1_;
   std::vector<WiegandReaderTrigger *> triggers_;
+
+  volatile unsigned long _cardTempHigh;
+  volatile unsigned long _cardTemp;
+  volatile unsigned long _lastWiegand;
+  volatile int _bitCount;
+  int _wiegandType;
+  unsigned long _code;
+
+  static void read_d0_(WiegandReader *reader);
+  static void read_d1_(WiegandReader *reader);
+  static unsigned long get_card_id_(volatile unsigned long *codehigh, volatile unsigned long *codelow, char bitlength);
+  static char translate_enter_escape_keypress_(char originalKeyPress);
+  static std::string get_code_hex_(std::string);
 };
 
 }  // namespace wiegand_reader
